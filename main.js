e165185016c64790c917e76ff92e499375853a83
William Doyle <mcbidebilly@gmail.com>
2021-06-15 18:56:41 -0400

    whatever

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+console.log('hello world');
+console.log("hello world")
+console.log('hello world')
+
+function foo() {
+	const arr = ['this', "is", `not`, "linted"];
+	const arr = ['nor', "is", `this`,];
+	console.log(`a + b = c`);
+}
